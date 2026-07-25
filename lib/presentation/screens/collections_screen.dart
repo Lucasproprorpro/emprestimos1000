e51ca7faedbee91b581aed59c6f9994d01f1b2e3
Lucasproprorpro <lucasgestor794@gmail.com>
2026-07-25 14:34:37 -0300

    Update collections_screen.dart

--- lib/presentation/screens/collections_screen.dart
+++ lib/presentation/screens/collections_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import '../../core/constants/enums.dart';
 import '../../core/theme/app_theme.dart';
 import '../../core/utils/formatters.dart';
 import '../providers/app_controller.dart';
